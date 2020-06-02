f40e439e5f035fe005197e693ec74316ad196e7a
Emporiusm <at19eden@gmail.com>
2020-06-03 01:01:30 +0800

    ISO932 Initial

--- ISO9362/BIC9362.py
+++ ISO9362/BIC9362.py
@@ -3,15 +3,12 @@
 # %%
 import pandas as pd
 import os
-
-
 # %%
-filedir = '/Users/edenmui/Documents/GitHub/ISO_libraries/ISO9362/AllCountries' + '/'
-filelist = os.listdir(filedir)
-files = [filedir + file for file in filelist if (file.endswith('.json') and len(file) == 7)]
+filedir = os.getcwd() + '/'
+jsondir = filedir + 'AllCountries' + '/'
+filelist = os.listdir(jsondir)
+files = [jsondir + file for file in filelist if (file.endswith('.json') and len(file) == 7)]
 
-
-# %%
 df = pd.DataFrame()
 for file in files:
     with open(file,'r') as content:
@@ -20,33 +17,18 @@ for file in files:
         df = df.append(jframe)
 df = df.reset_index(drop=True)
 
-
-# %%
 tempjson = filedir + 'list.json'
 tmpjs = df['list'].to_json(
     tempjson,
     orient='columns'
 )
 
-
-# %%
 lframe = pd.read_json(tempjson,orient='index')
 os.remove(tempjson)
-
-
-# %%
 lframe = lframe.reset_index(drop=True)
-
-
-# %%
 frame = df.join(lframe)
-
-
-# %%
-frame.to_csv(filedir + 'ISO9362_FULL_LIST.csv')
-
-
-# %%
+frame = frame.filter(items=['country_code','country','bank','city','branch','swift_code'])
+frame.to_csv(filedir + 'ISO9362 complete.csv')
 exit
 
 
